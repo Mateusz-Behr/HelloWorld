fb741c58c0da388c18b960987fe2bc47ad14053b
behrm <behrm@DESKTOP-SO5Q061>
2023-03-21 21:28:59 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HellWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam się Mateusz Behr.");
+            
+        }
+
+    }      
+}
